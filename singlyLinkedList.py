c9222a38e941c3e960d82b9ff7407209ae298bc6
Al-ImranRony <imranrony687@gmail.com>
2020-06-29 01:49:15 +0600

    Linked List easy way !

---
+++ singlyLinkedList.py
@@ -0,0 +1,72 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = Node()
+
+    def append(self, data):
+        new_node = Node(data)
+        cur = self.head
+        while cur.next != None:
+            cur = cur.next
+        cur.next = new_node
+
+    def length(self):
+        cur = self.head
+        total = 0
+        while cur.next != None:
+            total += 1
+            cur = cur.next
+        return total
+
+    def display(self):
+        elems = []
+        cur_node = self.head
+        while cur_node.next != None:
+            cur_node = cur_node.next
+            elems.append(cur_node.data)
+        print(elems)
+
+    def get(self, index):
+        if index >= self.length():
+            print ("Error: 'Get' index out of range.")
+            return None
+        cur_idx = 0
+        cur_node = self.head
+        while True:
+            cur_node = cur_node.next
+            if cur_idx == index:
+                return cur_node.data
+            cur_idx += 1
+
+    def erase(self, index):
+        if index >= self.length():
+            print("Error: 'Erase' index out of range.")
+            return 
+        cur_idx = 0
+        cur_node = self.head
+        while True:
+            last_node = cur_node
+            cur_node = cur_node.next
+            if cur_idx == index:
+                last_node.next = cur_node.next
+                return
+            cur_idx += 1
+
+the_list = LinkedList()
+
+the_list.display()
+
+the_list.append(1)
+the_list.append(3)
+the_list.append(7)
+the_list.append(33)
+
+the_list.display()
+print("Element at 2nd index:", the_list.get(2))
+
+the_list.erase(1)
+the_list.display()
