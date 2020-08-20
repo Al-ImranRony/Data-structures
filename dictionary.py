acd37d26854811b094ae49293b75faac37208a49
Al-ImranRony <imranrony687@gmail.com>
2020-08-20 23:18:13 +0600

    M: Merging dict shortcuts

--- dictionary.py
+++ dictionary.py
@@ -2,9 +2,22 @@
 
 counts = dict()
 names = ["Rony", "Imran", "Ali", "Rony"]
+fames = {'Cool': 1, 'Patient': 2, 'Ambisious': 3}
 for name in names:
     if name not in counts:
         counts[name] = 1
     else:
-        counts[name] = counts.get(name, 0) + 1  # Ignoring traceback by 'get' method
+        counts[name] = counts.get(name, 0) + 1  # Ignoring traceback by 'get' method, see below !
 print(counts)
+
+'''
+When "get()" is called it checks if the given key exists in the dict.
+- If it does exist, the value for that key is returned.
+- If it does not exist then the value of the default argument is returned instead
+'''
+
+
+# Merging 2 dictionaries
+
+mergedDict = {**counts, **fames}
+print(mergedDict)
