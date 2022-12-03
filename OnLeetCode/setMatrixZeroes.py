00a6fb1568ad2ada6663f3a976b912a98398dc00
Al-ImranRony <imranrony687@gmail.com>
2022-12-03 23:53:54 +0600

    Set matrix zeroes - optimized inplace solution.

--- OnLeetCode/setMatrixZeroes.py
+++ OnLeetCode/setMatrixZeroes.py
@@ -1,27 +1,35 @@
 '''
 Tech Interview Prep Group task - Session 1.
+Set Matrix Zero in Python
 '''
 
 
 givenMatrix = [[1,1,0], [1,1,1], [0,1,1]]
-ln = (len(givenMatrix))
-mi, mj, lenz = [-1]*ln, [-1]*ln, 0
+row = colum = (len(givenMatrix))
+mi, mj, roz, coz = [-1]*row, [-1]*colum, [-1]*row, [-1]*colum
  
-for i in range(ln):
-    for j in range(ln):
+for i in range(row):
+    for j in range(colum):
         if (givenMatrix[i][j] == 0):
             mi[i], mj[j] = i, j
-            # lenz += 1
+            roz[i], coz[j] = 0, 0
+
+'''This brute force solution needs O(m*n + m*n*k) time and O(n) extra space.'''
+# for i in range(row):
+#     for j in range(colum):
+#         for k in range(colum):
+#             if (i == mi[k] or j == mj[k]):
+#                 givenMatrix[i][j] = 0
+
+'''Time complexity reduced to: O(m*n + m*n) with O(n) extra space.'''
+for i in range(row):
+    for j in range(colum):
+        if (givenMatrix[i][j] != 0 and (roz[i] == 0 or coz[j] == 0)):
+            givenMatrix[i][j] = 0
 
 
-for i in range(ln):
-    for j in range(ln):
-        for k in range(ln):
-            if (i == mi[k] or j == mj[k]):
-                givenMatrix[i][j] = 0
-
-'''This brute force solution needs O(2m*n + k) time and O(n) extra space.'''
-for i in range(ln):
-    for j in range(ln):
+# Show
+for i in range(row):
+    for j in range(colum):
         print("|", givenMatrix[i][j], end=" |")
     print("\n")         
