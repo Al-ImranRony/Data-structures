fc502aa041a3161539db5d73a4f9aa9e86333994
Al-ImranRony <imranrony687@gmail.com>
2023-05-06 21:59:24 +0600

    Applied next permutation algo with step by step explanation

--- OnLeetCode/nextPermutaion.py
+++ OnLeetCode/nextPermutaion.py
@@ -7,6 +7,7 @@ from itertools import permutations
 
 
 n = 3
+
 # arrInput = list(map(int, input("Enter the list numbers separated by space :").strip().split()))[:n]
 # inputAsTuple = tuple(arrInput)
 # resultID = -1
@@ -25,22 +26,46 @@ n = 3
 
 '''This brute force solution takes O(n!*N) time'''
 
-def nextPermutation(nums: list) -> list:
-    x, y, z = nums[0], nums[1], nums[2]
-    
-    for i in range(1, n+1):
-        for j in range(1, n+1):
-            for k in range(1, n+1):
-                if (i != j and j != k and k != i):
+def nextPermutation(nums: list) -> None:
+    # x, y, z = nums[0], nums[1], nums[2]
+    # for i in range(1, n+1):
+    #     for j in range(1, n+1):
+    #         for k in range(1, n+1):
+    #             if (i != j and j != k and k != i):
                     
-                    if (x == 0 and y == 0 and z == 0):
-                        nums[0], nums[1], nums[2] = i, j, k 
-                        return nums
-                    if ([i,j,k] == [x,y,z]):
-                        x, y, z = 0, 0, 0
+    #                 if (x == 0 and y == 0 and z == 0):
+    #                     nums[0], nums[1], nums[2] = i, j, k 
+    #                     return nums
+    #                 if ([i,j,k] == [x,y,z]):
+    #                     x, y, z = 0, 0, 0
                     
-    return sorted(nums)
+    # return sorted(nums)
 
+    '''The "Next Permutation Algo" applied with 4 steps'''
+#1
+    k = len(nums) - 1
+    while k and nums[k-1] >= nums[k]: 
+        k-=1
+#2
+#    l = k+1
+#3
+    if k:
+        low, high = k, len(nums)
+        while low < high :
+            mid = (low+high)//2
+            if nums[mid] <= nums[k-1]:
+                high = mid
+            else :
+                low = mid + 1
 
-print(nextPermutation([2,3,1]))
+        nums[k-1], nums[low-1] = nums[low-1], nums[k-1]
+#4
+    low, high = k, len(nums)-1
+    while low < high: 
+        nums[low], nums[high] = nums[high], nums[low]
+        low, high = low+1, high-1  
+
+    return nums
+
+print(nextPermutation([1, 2, 3]))
 print(nextPermutation([3,2,1]))
